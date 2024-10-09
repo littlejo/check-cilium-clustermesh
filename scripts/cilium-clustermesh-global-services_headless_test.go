1a6fa65b29c73cc066a1f0bfa88ec3c9b59ad5a4
Joseph Ligier <joseph.ligier@accenture.com>
2024-10-09 18:14:32 +0200

    factorizate

--- scripts/cilium-clustermesh-global-services_headless_test.go
+++ scripts/cilium-clustermesh-global-services_headless_test.go
@@ -6,104 +6,23 @@ package test
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 	"strings"
 	"testing"
-	"text/template"
 	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
-	"github.com/gruntwork-io/terratest/modules/shell"
+
+	"scripts/lib"
 )
-
-func CreateConfigMapFile(n int, name string) string {
-	file := fmt.Sprintf("/tmp/cm-%s.yaml", name)
-	tmpl := `apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: cluster
-data:
-  CLUSTER: "{{.Cluster}}"
-  CLUSTERS_NUMBER: "{{.ClustersNumber}}"`
-
-	data := struct {
-		Cluster        string
-		ClustersNumber int
-	}{
-		Cluster:        name,
-		ClustersNumber: n,
-	}
-
-	outputFile, err := os.Create(file)
-	if err != nil {
-		panic(err)
-	}
-	defer outputFile.Close()
-
-	t := template.Must(template.New("configMap").Parse(tmpl))
-	err = t.Execute(outputFile, data)
-	if err != nil {
-		panic(err)
-	}
-
-	return file
-}
-
-func CreateFile(fileName string, content string) error {
-	file, err := os.Create(fileName)
-	if err != nil {
-		return fmt.Errorf("Error: %w", err)
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(content)
-	if err != nil {
-		return fmt.Errorf("Error: %w", err)
-	}
-
-	return nil
-}
-
-func WaitForPodLogs(t *testing.T, options *k8s.KubectlOptions, podName string, containerName string, maxRetries int, retryInterval time.Duration) (string, error) {
-	var logs string
-	pods := k8s.GetPod(t, options, podName)
-
-	for i := 0; i < maxRetries; i++ {
-		logs = k8s.GetPodLogs(t, options, pods, containerName)
-
-		if logs != "" {
-			return logs, nil
-		}
-
-		time.Sleep(retryInterval)
-	}
-
-	return "", fmt.Errorf("Impossible to retrieve after %d tries", maxRetries)
-}
-
-func GetKubeContexts(t *testing.T) ([]string, error) {
-	kubectlCmd := shell.Command{
-		Command: "kubectl",
-		Args:    []string{"config", "get-contexts", "-o", "name"},
-	}
-
-	output, err := shell.RunCommandAndGetStdOutE(t, kubectlCmd)
-	if err != nil {
-		return nil, fmt.Errorf("Error: %v", err)
-	}
-
-	contexts := strings.Split(strings.TrimSpace(output), "\n")
-	return contexts, nil
-}
 
 func TestCiliumClusterMeshGlobalServiceHeadless(t *testing.T) {
 	t.Parallel()
 
-	contexts, err := GetKubeContexts(t)
+	contexts, err := lib.GetKubeContexts(t)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -115,7 +34,7 @@ func TestCiliumClusterMeshGlobalServiceHeadless(t *testing.T) {
 	namespaceName := fmt.Sprintf("cilium-cmesh-test-%s", strings.ToLower(random.UniqueId()))
 
 	for i, c := range contexts {
-		cm := CreateConfigMapFile(cluster_number, c)
+		cm := lib.CreateConfigMapFile(cluster_number, c)
 		cmResourcePath, err := filepath.Abs(cm)
 		file_web := "../web-server/k8s/headless-svc.yaml"
 		if i == 0 {
@@ -149,10 +68,10 @@ func TestCiliumClusterMeshGlobalServiceHeadless(t *testing.T) {
 		options := k8s.NewKubectlOptions(c, "", namespaceName)
 		pods := k8s.GetPod(t, options, podName)
 		k8s.WaitUntilPodAvailable(t, options, podName, 60, time.Duration(1)*time.Second)
-		WaitForPodLogs(t, options, podName, containerName, cluster_number, time.Duration(10)*time.Second)
+		lib.WaitForPodLogs(t, options, podName, containerName, cluster_number, time.Duration(10)*time.Second)
 		logs := k8s.GetPodLogs(t, options, pods, containerName)
 		t.Log("Value of logs is:", logs)
-		CreateFile(fmt.Sprintf("/tmp/runner-%s.log", c), logs)
+		lib.CreateFile(fmt.Sprintf("/tmp/runner-%s.log", c), logs)
 		numberOfLines := strings.Count(logs, "\n") + 1
 		require.Equal(t, numberOfLines, 1)
 		require.Contains(t, logs, "100")
