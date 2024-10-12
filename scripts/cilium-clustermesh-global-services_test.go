e305baf189a234baaf92d223e7581491e2b78301
Joseph Ligier <joseph.ligier@accenture.com>
2024-10-12 23:45:08 +0200

    Improve code

--- scripts/cilium-clustermesh-global-services_test.go
+++ scripts/cilium-clustermesh-global-services_test.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
@@ -28,49 +29,52 @@ func TestCiliumClusterMeshGlobalService(t *testing.T) {
 		return
 	}
 	cluster_number := len(contexts)
-	podName := "script-runner"
-	containerName := "script-container"
+	deploymentName := "client"
+	containerName := "client"
 
 	namespaceName := fmt.Sprintf("cilium-cmesh-test-%s", strings.ToLower(random.UniqueId()))
 
 	for _, c := range contexts {
 		cm := lib.CreateConfigMapFile(cluster_number, c)
 		cmResourcePath, err := filepath.Abs(cm)
-		webResourcePath, err := filepath.Abs("../web-server/k8s/pod-svc.yaml")
+		webResourcePath, err := filepath.Abs("../web-server/k8s/common/web-app.yaml")
 		require.NoError(t, err)
 
 		options := k8s.NewKubectlOptions(c, "", namespaceName)
 
 		k8s.CreateNamespace(t, options, namespaceName)
 		defer k8s.DeleteNamespace(t, options, namespaceName)
-		//defer k8s.KubectlDelete(t, options, webResourcePath)
+		defer k8s.KubectlDelete(t, options, webResourcePath)
 
 		k8s.KubectlApply(t, options, cmResourcePath)
 		k8s.KubectlApply(t, options, webResourcePath)
 	}
 
 	options := k8s.NewKubectlOptions(contexts[len(contexts)-1], "", namespaceName)
-	k8s.WaitUntilPodAvailable(t, options, "go-web-server-pod", 60, time.Duration(1)*time.Second)
+	k8s.WaitUntilDeploymentAvailable(t, options, "web-app", 60, time.Duration(1)*time.Second)
 
 	for _, c := range contexts {
-		clientResourcePath, err := filepath.Abs("../web-server/k8s/pod-script.yaml")
+		clientResourcePath, err := filepath.Abs("../web-server/k8s/global-load-balancing/client.yaml")
 		require.NoError(t, err)
 
 		options := k8s.NewKubectlOptions(c, "", namespaceName)
 
-		//defer k8s.KubectlDelete(t, options, clientResourcePath)
+		defer k8s.KubectlDelete(t, options, clientResourcePath)
 
 		k8s.KubectlApply(t, options, clientResourcePath)
 	}
 
 	for _, c := range contexts {
 		options := k8s.NewKubectlOptions(c, "", namespaceName)
-		pods := k8s.GetPod(t, options, podName)
-		k8s.WaitUntilPodAvailable(t, options, podName, 60, time.Duration(1)*time.Second)
-		lib.WaitForPodLogs(t, options, podName, containerName, cluster_number, time.Duration(10)*time.Second)
-		logs := k8s.GetPodLogs(t, options, pods, containerName)
+		filters := metav1.ListOptions{
+			LabelSelector: "app=client",
+		}
+		k8s.WaitUntilDeploymentAvailable(t, options, deploymentName, 60, time.Duration(1)*time.Second)
+		pod := k8s.ListPods(t, options, filters)[0]
+		lib.WaitForPodLogs(t, options, pod.Name, containerName, cluster_number, time.Duration(10)*time.Second)
+		logs := k8s.GetPodLogs(t, options, &pod, containerName)
 		t.Log("Value of logs is:", logs)
-		lib.CreateFile(fmt.Sprintf("/tmp/runner-%s.log", c), logs)
+		lib.CreateFile(fmt.Sprintf("/tmp/client-%s.log", c), logs)
 		numberOfLines := strings.Count(logs, "\n") + 1
 		require.Equal(t, numberOfLines, cluster_number)
 		for _, c := range contexts {
