903ee135e4d1722b5259c3d9ffaebfc7e4f58f33
Joseph Ligier <joseph.ligier@accenture.com>
2024-10-23 12:41:27 +0200

    Improve check-cilium.sh

--- scripts/check-cilium.sh
+++ scripts/check-cilium.sh
@@ -22,9 +22,9 @@ done
 for c in $(cat $ctx)
 do
 	cilium status -o json --context $c > $dir/$c/cilium-status.json
-	$python ./cilium-status.py $dir/$c/cilium-status.json $clusters_n | tee $dir/$c/cilium-status-json.log
+	cilium-status.py $dir/$c/cilium-status.json $clusters_n | tee $dir/$c/cilium-status-json.log
 	cilium clustermesh status -o json --context $c > $dir/$c/cilium-clustermesh-status-connectivity.json
-	$python ./cilium-clustermesh-status.py $dir/$c/cilium-clustermesh-status-connectivity.json $clusters_n | tee $dir/$c/cilium-clustermesh-status-connectivity-json.log
+	cilium-clustermesh-status.py $dir/$c/cilium-clustermesh-status-connectivity.json $clusters_n | tee $dir/$c/cilium-clustermesh-status-connectivity-json.log
 done
 
 cat $ctx | xargs -P $clusters_n -I {} bash -c "cilium connectivity test --context {} | tee $dir/{}/connectivity-test.log"
